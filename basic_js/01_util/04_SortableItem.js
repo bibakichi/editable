349e608724721565b5bbfad814094ef4bb3a1a0b
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-03 18:57:55 +0900

    ドラッグ＆ドロップをON/OFFできるように

--- basic_js/01_util/04_SortableItem.js
+++ basic_js/01_util/04_SortableItem.js
@@ -22,14 +22,9 @@ class SortableItem {
         //
         // outerElementの移動（ドラッグ）が始まった時のイベント
         const outerElement = this._outerElement;
-        outerElement.ondragstart = function (event) {
-            event.dataTransfer.setData("outerId", outerElement.id);  // データ転送用のデータをセット
-            event.dataTransfer.setData("jsonData", JSON.stringify(jsonData));      // データ転送用のデータをセット
-            outerElement.style.opacity = 0;
-            const dropAreasList = document.getElementsByClassName('sortable_item_drop_areas');
-            for (const dropAreas of dropAreasList) {
-                //dropAreas.style.display = 'block';
-            }
+        outerElement.ondragstart = (event) => _handleDragStart(event, jsonData);
+        outerElement.ondragend = (event) => {
+            outerElement.style.zIndex = 999;
         };
     }
     get jsonData() { return this._jsonData; }
@@ -153,6 +148,18 @@ class SortableItem {
         }
     }
 
+    _handleDragStart(event, jsonData) {
+        event.dataTransfer.setData("outerId", outerElement.id);  // データ転送用のデータをセット
+        event.dataTransfer.setData("jsonData", JSON.stringify(jsonData));      // データ転送用のデータをセット
+        outerElement.style.opacity = 0;
+        const dropAreasList = document.getElementsByClassName('sortable_item_drop_areas');
+        for (const dropAreas of dropAreasList) {
+            dropAreas.style.display = 'block';
+            dropAreas.style.zIndex = 1;
+        }
+        this._outerElement.style.zIndex = 999;
+    }
+
     constructor({ isDragOnly = false, isDropOnly = false, isEnable = true, }) {
         const id = uuid();
         sortableItems[id] = this;
@@ -249,15 +256,7 @@ class SortableItem {
         }
         if (!isDropOnly) {
             // outerElementの移動（ドラッグ）が始まった時のイベント
-            outerElement.ondragstart = function (event) {
-                event.dataTransfer.setData("outerId", outerElement.id);  // データ転送用のデータをセット
-                event.dataTransfer.setData("jsonData", "{}");      // データ転送用のデータをセット
-                outerElement.style.opacity = 0;
-                const dropAreasList = document.getElementsByClassName('sortable_item_drop_areas');
-                for (const dropAreas of dropAreasList) {
-                    dropAreas.style.display = 'block';
-                }
-            };
+            outerElement.ondragstart = (event) => _handleDragStart(event, {});
             //
             // outerElementを移動（ドラッグ）している最中に連続してイベント発生
             outerElement.addEventListener("drag", function (event) {
