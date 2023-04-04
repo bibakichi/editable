8d2c806bcf65a4fe2b8c0003f7846ab27b3785c7
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-04 15:54:29 +0900

    画像もドロップできるように

---
+++ basic_js/02_defaultPlugins/P.js
@@ -0,0 +1,49 @@
+//#########################################################################################
+plugins["P"] = {
+    "viewer": {
+        "renderLight": async function (blockId, saveData) {
+            const element = document.createElement('p');
+            element.id = blockId;
+            element.innerText = saveData.text ?? "本文";
+            return element;
+        },
+        "changeEditMode": async function (blockId) {
+            const element = document.getElementById(blockId);
+            element.addEventListener('click', (event) => {
+                //
+                // この１文がなかったら、
+                // 編集中のテキストの先頭をクリックすると、カーソルが自動的に最後まで移動してしまう
+                if (element.isContentEditable) return;
+                //
+                element.contentEditable = true;
+                element.focus();
+                // １文字以上の場合
+                if (element.childNodes.length > 0) {
+                    // カーソル位置を最後にもっていく
+                    const range = document.createRange();
+                    const sel = window.getSelection();
+                    range.setStart(element.childNodes[0], element.innerText.length);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            });
+            element.addEventListener('focusout', (event) => {
+                element.contentEditable = false;
+            });
+        },
+        "saveBlock": async function (blockId, pastSaveData) {
+            const element = document.getElementById(blockId);
+            return {
+                text: element.textContent,
+            };
+        },
+    },
+    "toolbox": {
+        "render": async function (saveData) {
+            const toolboxElement = document.createElement('p');
+            toolboxElement.innerText = saveData.text ?? '本文';
+            return toolboxElement;
+        },
+    },
+}
---
+++ basic_js/02_defaultPlugins/Padding.js
@@ -0,0 +1,19 @@
+//#########################################################################################
+plugins["Padding"] = {
+    "viewer": {
+        "renderLight": async function (blockId, saveData) {
+            const element = document.createElement('div');
+            element.id = blockId;
+            element.style.width = "100%";
+            element.style.height = "40px";
+            return element;
+        },
+    },
+    "toolbox": {
+        "render": async function (saveData) {
+            const toolboxElement = document.createElement('h4');
+            toolboxElement.innerText = saveData.text ?? '空白';
+            return toolboxElement;
+        },
+    },
+}
