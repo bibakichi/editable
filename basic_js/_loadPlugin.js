524e30859fc19cbdeb891455c8ad3778927127f1
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-17 14:51:54 +0900

    externals>jsとcsswo

--- basic_js/_loadPlugin.js
+++ basic_js/_loadPlugin.js
@@ -4,9 +4,10 @@ var loadingPlugins = {};
 
 // プラグインファイルを読み込む
 async function _loadPlugin(blockType) {
-    if (plugins[blockType]) {
+    const plugin = plugins[blockType];
+    if (plugin) {
         if (isDebugPlugin) console.log("  既にプラグインは読み込み済みです。");
-        return plugins[blockType];
+        return plugin;
     }
     const url = './plugins/' + blockType + '.js?t=' + String(new Date().getTime());    //キャッシュ対策
     if (loadingPlugins[blockType] === true) {
@@ -36,26 +37,62 @@ async function _loadPlugin(blockType) {
     //
     loadingPlugins[blockType] = false;
     //
-    if (plugins[blockType]) {
+    if (plugin) {
         if (isDebugPlugin) console.log("  プラグイン読み込み成功");
     }
     else {
         console.error("  プラグインファイルが見つかりません");
         return null;
     }
-    if (typeof plugins[blockType].css === "function") {
+    if (typeof plugin.css === "function") {
         const styleTag = document.createElement('style');
         try {
-            styleTag.innerHTML = await plugins[blockType].css();
+            styleTag.innerHTML = await plugin.css();
         }
         catch (err) {
             alert(`プラグイン「${blockType}」の関数「css()」でエラーが発生しました。`);
             console.error(`プラグイン「${blockType}」の関数「css()」でエラーが発生しました。`);
             console.error(err);
-            return plugins[blockType];
+            return plugin;
         }
         // 作成したstyleタグを挿入
         document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
     }
-    return plugins[blockType];
+    if (typeof plugin?.externals?.css === "function") {
+        let urlList = [];
+        try {
+            urlList = await plugin.externals.css();
+        }
+        catch (err) {
+            alert(`プラグイン「${blockType}」の関数「externals.css()」でエラーが発生しました。`);
+            console.error(`プラグイン「${blockType}」の関数「externals.css()」でエラーが発生しました。`);
+            console.error(err);
+            return plugin;
+        }
+        for (const url of urlList) {
+            const styleTag = document.createElement('link');
+            styleTag.rel = "stylesheet";
+            styleTag.href = url;
+            // 作成したstyleタグを挿入
+            document.getElementsByTagName('head')[0].insertAdjacentElement('beforeend', styleTag);
+        }
+    }
+    if (typeof plugin?.externals?.js === "function") {
+        let urlList = [];
+        try {
+            urlList = await plugin.externals.js();
+        }
+        catch (err) {
+            alert(`プラグイン「${blockType}」の関数「externals.js()」でエラーが発生しました。`);
+            console.error(`プラグイン「${blockType}」の関数「externals.js()」でエラーが発生しました。`);
+            console.error(err);
+            return plugin;
+        }
+        for (const url of urlList) {
+            const scriptElement = document.createElement('script');
+            scriptElement.src = url;
+            document.body.appendChild(scriptElement);
+        }
+    }
+    return plugin;
 }
