949b121d8475e1f630f626a059a80356431402fb
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-18 20:32:05 +0900

    認証機能を追加

--- basic_js/09_microsoft/getMicrosoftProfile.js
+++ basic_js/09_microsoft/getMicrosoftProfile.js
@@ -13,7 +13,7 @@ async function _getMicrosoftProfile() {
         const iframeElement = document.createElement("iframe");
         const redirectUri = window.encodeURIComponent(window.location.href);
         iframeElement.src = `https://7mo.nl/login?redirect_uri=${redirectUri}`;
-        iframeElement
+        iframeElement.style.width = "100%";
         mainElement.replaceWith(iframeElement);
         //
         checkboxElement.checked = true;
