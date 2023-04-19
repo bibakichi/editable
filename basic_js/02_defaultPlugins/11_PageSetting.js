6f4d8b3696c308828069709fb21ced7fff716af1
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-19 13:53:12 +0900

    ログイン機能を追加

--- basic_js/02_defaultPlugins/11_PageSetting.js
+++ basic_js/02_defaultPlugins/11_PageSetting.js
@@ -28,14 +28,44 @@ plugins["PageSetting"] = {
             inputElement1.value = settings[0]?.title;
             divElement1.appendChild(inputElement1);
             inputElement1.addEventListener("input", () => {
+                document.title = inputElement1.value;
                 settings[0] = {
                     ...settings[0],
                     title: inputElement1.value,
                 };
             });
             //
+            //
             const divElement2 = document.createElement('div');
             mainElement.appendChild(divElement2);
+            //
+            const labelElement2 = document.createElement('label');
+            labelElement2.classList.add("switch");
+            divElement2.appendChild(labelElement2);
+            //
+            const spanElement2 = document.createElement('span');
+            spanElement2.innerText = "全画面表示";
+            labelElement2.appendChild(spanElement2);
+            //
+            const inputElement2 = document.createElement('input');
+            inputElement2.checked = settings[0]?.isFullSize;
+            labelElement2.appendChild(inputElement2);
+            inputElement2.addEventListener("change", () => {
+                settings[0] = {
+                    ...settings[0],
+                    isFullSize: inputElement1.checked,
+                };
+                const element = document.querySelector('#this_page_modal_trigger+.modal_overlay+.modal_outer');
+                if (settings[0].isFullSize) {
+                    element.classList.add('full_size');
+                }
+                else {
+                    element.classList.remove('full_size');
+                }
+            });
+            //
+            const sliderElement2 = document.createElement('span');
+            labelElement2.appendChild(sliderElement2);
             //
             const divElement3 = document.createElement('div');
             mainElement.appendChild(divElement3);
