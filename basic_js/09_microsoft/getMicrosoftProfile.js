02ace9a51699475fe78b74192201662d1fcac7e5
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-19 12:30:14 +0900

    ログイン機能を追加

--- basic_js/09_microsoft/getMicrosoftProfile.js
+++ basic_js/09_microsoft/getMicrosoftProfile.js
@@ -6,10 +6,23 @@ async function _getMicrosoftProfile() {
     }
     microsoftProfile = await loadMicrosoftProfile();
     if (!microsoftProfile) {
+        const { openButtonElement, mainElement, checkboxElement } = createModal();
+        //
+        const h2Element = document.createElement("h2");
+        h2Element.style.textAlign = "center";
+        h2Element.innerText = "ログインをする必要があります";
+        mainElement.appendChild(h2Element);
+        //
+        const buttonElement = document.createElement("a");
+        buttonElement.innerText = "ログイン";
+        mainElement.appendChild(buttonElement);
+        //
         const redirectUri = new URL(window.location.href);
         redirectUri.searchParams.set("editmode", 1);
         const redirectUriText = window.encodeURIComponent(redirectUri.toString());
-        window.location.href = `https://7mo.nl/login?redirect_uri=${redirectUriText}`;
+        buttonElement.href = `https://7mo.nl/login?redirect_uri=${redirectUriText}`;
+        //
+        checkboxElement.checked = true;
     }
     return microsoftProfile;
 }
