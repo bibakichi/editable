e4bb7c9fdd3d7bdfabd6a7777b04d7a5388ec59f
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-19 13:40:46 +0900

    ログイン機能を追加

--- basic_js/02_defaultPlugins/11_PageSetting.js
+++ basic_js/02_defaultPlugins/11_PageSetting.js
@@ -3,25 +3,47 @@ plugins["PageSetting"] = {
     "isDefault": true,
     "viewer": {
         "renderLight": async function (blockId, saveData) {
-            const buttonElement = document.createElement('button');
-            buttonElement.id = blockId;
-            buttonElement.innerText = "ページ設定";
-            buttonElement.addEventListener("click", () => {
-                window.localStorage.removeItem("graphApiInfo");
-                window.location.href = "https://login.microsoftonline.com/logout.srf";
-            });
-            return buttonElement;
+            return document.createElement("div");
         },
     },
     "toolbox": {
         "render": async function (saveData) {
-            const buttonElement = document.createElement('button');
-            buttonElement.innerText = "ログアウト";
-            buttonElement.addEventListener("click", () => {
-                window.localStorage.removeItem("graphApiInfo");
-                window.location.href = "https://login.microsoftonline.com/logout.srf";
-            });
-            return buttonElement;
+            const { openButtonElement, mainElement } = createModal();
+            openButtonElement.innerText = "ページ設定";
+            //
+            const titleElement = document.createElement('h2');
+            titleElement.innerText = "ページ設定";
+            titleElement.style.textAlign = "center";
+            titleElement.style.margin = "50px 0";
+            mainElement.appendChild(titleElement);
+            //
+            const divElement1 = document.createElement('div');
+            mainElement.appendChild(divElement1);
+            //
+            const labelElement1 = document.createElement('label');
+            labelElement1.innerText = "タイトル";
+            divElement1.appendChild(labelElement1);
+            //
+            const inputElement1 = document.createElement('input');
+            inputElement1.value = settings[0]?.title;
+            divElement1.appendChild(inputElement1);
+            //
+            const divElement2 = document.createElement('div');
+            mainElement.appendChild(divElement2);
+            //
+            const divElement3 = document.createElement('div');
+            mainElement.appendChild(divElement3);
+            //
+            const divElement4 = document.createElement('div');
+            mainElement.appendChild(divElement4);
+            //
+            const divElement5 = document.createElement('div');
+            mainElement.appendChild(divElement5);
+            //
+            const divElement6 = document.createElement('div');
+            mainElement.appendChild(divElement6);
+            //
+            return openButtonElement;
         },
     },
 }
