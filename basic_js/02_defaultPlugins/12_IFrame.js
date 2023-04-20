42e9807ef91d2a8ad3fc38b4478de695d0dde334
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-20 19:41:38 +0900

    iframe機能を追加

--- basic_js/02_defaultPlugins/12_IFrame.js
+++ basic_js/02_defaultPlugins/12_IFrame.js
@@ -17,7 +17,7 @@ plugins["IFrame"] = {
             iframeElement.style.width = "100%";
             iframeElement.style.height = saveData?.height ?? "1000px";
             iframeElement.style.minHeight = "200px";
-            iframeElement.src = saveData?.url ?? "https://docs.google.com/forms/d/e/1FAIpQLSfbK4debgfo9LMG5HzPY8E0H1RoJX7rFdR2NhtFf3zSmFEZvg/viewform?embedded=true";
+            iframeElement.src = saveData?.url ?? "";
             outerElement.appendChild(iframeElement);
             //
             return outerElement;
