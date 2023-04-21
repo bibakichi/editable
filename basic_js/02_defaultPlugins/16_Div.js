dc329e388136ca4a73f9b81ed49ed21598993169
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-21 09:44:55 +0900

    画像ブロックを改良

---
+++ basic_js/02_defaultPlugins/16_Div.js
@@ -0,0 +1,50 @@
+//#########################################################################################
+plugins["Div"] = {
+    "isDefault": true,
+    "viewer": {
+        "renderLight": async function (blockId, saveData) {
+            const element = document.createElement('div');
+            element.id = blockId;
+            element.innerHTML = saveData?.text ?? "上下に余白の無い本文";
+            return element;
+        },
+        "changeEditMode": async function (blockId) {
+            const element = document.getElementById(blockId);
+            element.addEventListener('click', (event) => {
+                //
+                // この１文がなかったら、
+                // 編集中のテキストの先頭をクリックすると、カーソルが自動的に最後まで移動してしまう
+                if (element.isContentEditable) return;
+                //
+                element.contentEditable = true;
+                element.focus();
+                // １文字以上の場合
+                if (element.childNodes.length > 0) {
+                    // カーソル位置を最後にもっていく
+                    const range = document.createRange();
+                    const sel = window.getSelection();
+                    range.setStart(element.childNodes[0], element.innerHTML.length);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            });
+            element.addEventListener('focusout', (event) => {
+                element.contentEditable = false;
+            });
+        },
+        "saveBlock": async function (blockId, pastSaveData) {
+            const element = document.getElementById(blockId);
+            return {
+                text: element.innerHTML,
+            };
+        },
+    },
+    "toolbox": {
+        "render": async function (saveData) {
+            const toolboxElement = document.createElement('div');
+            toolboxElement.innerText = saveData?.text ?? '上下に余白の無い本文';
+            return toolboxElement;
+        },
+    },
+}
