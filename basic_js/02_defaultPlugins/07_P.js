e8ea03fe25d6c38c1a6c699622f95f7d017ff3f4
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-23 02:40:58 +0900

    リッチテキストエディタ「Quill」を導入

--- basic_js/02_defaultPlugins/07_P.js
+++ basic_js/02_defaultPlugins/07_P.js
@@ -28,15 +28,12 @@ plugins["P"] = {
                 modules: {
                     toolbar: [
                         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-                        ['blockquote', 'code-block'],
 
-                        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
                         [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
                         [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
                         [{ 'direction': 'rtl' }],                         // text direction
 
-                        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                        [{ 'size': ['小', '中', '大', '特大'] }],  // custom dropdown
 
                         [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
                         [{ 'font': [] }],
