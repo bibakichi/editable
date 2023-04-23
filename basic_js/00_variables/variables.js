8ca4c618b709491d39d0877082adeffa5e4af58d
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-04-23 15:16:30 +0900

    minify化

--- basic_js/00_variables/variables.js
+++ basic_js/00_variables/variables.js
@@ -5,7 +5,7 @@ const settings = [];
 var isEditMode = false; // この変数がtrueのときは、モーダルのアニメーションを表示しない
 
 const isDebugTree = false;
-const isDebugPlugin = true;
+const isDebugPlugin = false;
 
 let microsoftProfile = null;
 let isLoginRequired = false;
