55503afaacf024d51b09849720ecba878470f63b
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-05-31 16:29:11 +0900

    HTML素通し機能を追加

---
+++ basic_js/02_defaultPlugins/19_StaticHTML.js
@@ -0,0 +1,56 @@
+//#########################################################################################
+
+function rendarStaticHTML({ blockId, saveData }) {
+    const outerElement = document.createElement("div");
+    outerElement.id = blockId;
+    if (!saveData?.html) return outerElement;
+    outerElement.innerHTML = saveData.html;
+    const elements = outerElement.querySelectorAll(":scope>*");
+    if (elements.length == 0) return outerElement;
+    if (elements.length >= 2) return outerElement;
+    if (
+        (elements[0].tagName == "DIV") &&
+        (elements[0].classList.keys.length == 0) &&
+        (!elements[0].id) &&
+        (elements[0].style.length == 0)
+    ) {
+        // もし一番外側（elements[0]）が、ただの「div」だったら
+        const innerElements = elements[0].querySelectorAll(":scope>*");
+        if (innerElements.length == 1) {
+            if (!innerElements[0].id) {
+                innerElements[0].id = blockId;
+                return innerElements[0];
+            }
+        }
+    }
+    if (!elements[0].id) {
+        elements[0].id = blockId;
+        return elements[0];
+    }
+    return outerElement;
+}
+
+plugins["StaticHTML"] = {
+    "isDefault": true,
+    "toolbox": {
+        "render": async function (saveData) {
+            const iframeElement = document.createElement('iframe');
+            iframeElement.src = "data:text/html," + ((saveData?.html) ?? "");
+            return iframeElement;
+        },
+    },
+    "viewer": {
+        "renderLight": async function (blockId, saveData) {
+            return rendarStaticHTML({ blockId, saveData });
+        },
+        "changeEditMode": async function (blockId, saveData) {
+
+        },
+        "saveBlock": async function (blockId, pastSaveData) {
+            const element = document.getElementById(blockId);
+            return {
+                html: element.outerHTML,
+            };
+        }
+    }
+}
--- basic_js/04_mainContents/initMainContents.js
+++ basic_js/04_mainContents/initMainContents.js
@@ -5,28 +5,19 @@ async function initMainContents() {
     const mainContents = document.getElementById('main_contents');
     for (const outerElement of mainContents.querySelectorAll(":scope>*")) {
         const jsonElement = outerElement.querySelector('.json');
-        if (!jsonElement) {
-            //
-            // HTML素通し機能 ここから
-            const sortableItem = new SortableItem({
-                isEnable: false,
-                enableCopy: false,
-            });
-            const innerElements = outerElement.querySelectorAll(":scope>*");
-            if (innerElements.length == 1) {
-                sortableItem.innerElement = innerElements[0];
+        let saveData;
+        if (jsonElement) {
+            try {
+                saveData = JSON.parse(jsonElement.textContent);
             }
-            else {
-                for (const e of innerElements) {
-                    sortableItem.innerElement.appendChild(e);
-                }
-            }
-            outerElement.replaceWith(sortableItem.outerElement);
-            continue;
-            // HTML素通し機能 ここまで
-            //
+            catch (err) { }
         }
-        const saveData = JSON.parse(jsonElement.textContent);
+        if (!saveData) {
+            saveData = {
+                blockType: "StaticHTML",
+                html: outerElement.outerHTML,
+            };
+        }
         const outerElement2 = await _renderHeavy(saveData);
         if (outerElement2) {
             outerElement.replaceWith(outerElement2);
