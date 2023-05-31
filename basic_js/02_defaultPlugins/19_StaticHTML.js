ae1e78ad228dca44ddc3ba4272adf9846079e903
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-05-31 19:14:29 +0900

    HTML素通し機能を追加

--- basic_js/02_defaultPlugins/19_StaticHTML.js
+++ basic_js/02_defaultPlugins/19_StaticHTML.js
@@ -1,15 +1,19 @@
 //#########################################################################################
 
-function rendarStaticHTML({ blockId, saveData }) {
-    let outerElement = document.createElement("div");
-    if (!saveData?.html) return outerElement;
-    outerElement.innerHTML = saveData.html;
-    while (true) {
-        const elements = outerElement.querySelectorAll(":scope>*");
-        if (elements.length == 0) return outerElement;
-        if (elements.length >= 2) return outerElement;
-        return elements[0];
+function _getStaticHTML(element) {
+    const children = element.querySelectorAll(":scope>*");
+    if (children.length == 0) return element;
+    if (children.length >= 2) return element;
+    if (
+        (children[0].tagName == "DIV") &&
+        (children[0].classList.keys.length == 0) &&
+        (!children[0].id) &&
+        (children[0].style.length == 0)
+    ) {
+        // もし一番外側（elements[0]）が、ただの「div」だったら;
+        return _getStaticHTML(children[0]);
     }
+    return element;
 }
 
 plugins["StaticHTML"] = {
@@ -17,13 +21,22 @@ plugins["StaticHTML"] = {
     "toolbox": {
         "render": async function (saveData) {
             const iframeElement = document.createElement('iframe');
+            iframeElement.style.pointerEvents = "none";
             iframeElement.src = "data:text/html," + ((saveData?.html) ?? "");
             return iframeElement;
         },
     },
     "viewer": {
-        "renderLight": async function (blockId, saveData) {
-            return rendarStaticHTML({ blockId, saveData });
+        "renderHeavy": async function (blockId, saveData) {
+            const element = document.createElement("div");
+            if (!saveData?.html) {
+                element.id = blockId;
+                return element;
+            }
+            element.innerHTML = saveData.html;
+            const element2 = _getStaticHTML(element);
+            element2.id = blockId;
+            return element2;
         },
         "changeEditMode": async function (blockId, saveData) {
 
@@ -33,6 +46,14 @@ plugins["StaticHTML"] = {
             return {
                 html: element.innerHTML,
             };
-        }
+        },
+        "renderLight": async function (blockId, saveData) {
+            const element = document.createElement("div");
+            if (!saveData?.html) {
+                return element;
+            }
+            element.innerHTML = saveData.html;
+            return _getStaticHTML(element);
+        },
     }
 }
