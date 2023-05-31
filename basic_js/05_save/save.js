99e817e3325b8a2098fd31f56980af3f28d7ff97
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-05-31 23:34:45 +0900

    バージョン2最初のコミット

--- basic_js/05_save/save.js
+++ basic_js/05_save/save.js
@@ -38,7 +38,6 @@ async function allSave(isDownload) {
         }
     }
     //
-    /*
     const externalFiles = [
         ...document.querySelectorAll("head>style:not(.Do_not_store_in_HTML)"),
         ...document.querySelectorAll("head>script:not(.Do_not_store_in_HTML)"),
@@ -49,7 +48,6 @@ async function allSave(isDownload) {
         externalFilesText += "\n        " + externalFile.outerHTML;
     }
     console.log(externalFilesText);
-    */
     //
     const htmlCode = _generateHTML({
         title: settings[0]?.title ?? "",
@@ -58,7 +56,7 @@ async function allSave(isDownload) {
         jsZipPath: document.getElementById("jszip").getAttribute('src'),
         isFullSize: settings[0]?.isFullSize,
         faviconsFolderPath: 'https://mono-file.s3.ap-northeast-1.amazonaws.com/favicons/',
-        externalFiles: '',//externalFilesText,
+        externalFiles: externalFilesText,
     });
 
     const uri = new URL(window.location.href);
