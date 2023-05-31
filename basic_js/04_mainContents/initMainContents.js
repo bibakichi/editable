4b16c2434b396ff51a2508361884f26d8a6ff571
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-05-31 12:39:20 +0900

    HTML素通し機能を追加

--- basic_js/04_mainContents/initMainContents.js
+++ basic_js/04_mainContents/initMainContents.js
@@ -5,7 +5,23 @@ async function initMainContents() {
     const mainContents = document.getElementById('main_contents');
     for (const outerElement of mainContents.querySelectorAll(":scope>*")) {
         const jsonElement = outerElement.querySelector('.json');
-        if (!jsonElement) continue;
+        if (!jsonElement) {
+            const sortableItem = new SortableItem({
+                isEnable: isEditable,
+                enableCopy: false,
+            });
+            const innerElements = outerElement.querySelectorAll(":scope>*");
+            if (innerElements.length == 1) {
+                sortableItem.innerElement = innerElements[0];
+            }
+            else {
+                for (const e of innerElements) {
+                    sortableItem.innerElement.appendChild(e);
+                }
+            }
+            outerElement.replaceWith(sortableItem.outerElement);
+            continue;
+        }
         const saveData = JSON.parse(jsonElement.textContent);
         const outerElement2 = await _renderHeavy(saveData);
         if (outerElement2) {
