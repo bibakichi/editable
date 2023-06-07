44632940a031a134121bc3f587ec506f0656e931
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-06-07 15:43:33 +0900

    manifest.jsonを追加

--- basic_js/04_mainContents/onDropBrotherFile.js
+++ basic_js/04_mainContents/onDropBrotherFile.js
@@ -22,7 +22,7 @@ async function onDropBrotherFile({ file, isBefore, sortableItem }) {
             pluginsBuffer = {};
             text = text.replace("plugins", "pluginsBuffer");
             const scriptElement = document.createElement('script');
-            scriptElement.classList.add("Do_not_store_in_HTML"); s
+            scriptElement.classList.add("Do_not_store_in_HTML");
             scriptElement.innerHTML = text;
             document.body.appendChild(scriptElement);
             await _sleep(1000);
