f08f23d0f6fae910fd24249a3f327608dc7820b0
bibakichi <k.kimura.qs@cis.fukuoka-u.ac.jp>
2023-11-08 11:29:39 +0900

    親ページと見出しを連動させる機能を開発中

--- basic_js/02_defaultPlugins/20_ChildHeadlines.js
+++ basic_js/02_defaultPlugins/20_ChildHeadlines.js
@@ -13,36 +13,41 @@ plugins["ChildHeadlines"] = {
     "viewer": {
         "renderHeavy": async function (blockId, saveData) {
             const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
-            while (!settings[0]) {
-                await sleep(500);
-            }
             const listElement = document.createElement("div");
             listElement.style.minHeight = "200px";
+            listElement.innerHTML = "読み込み中…";
             listElement.id = blockId;
-            const childPages = {
-                ...(settings[0]?.childPages ?? {}),
-            };
-            for (const folderName in childPages) {
-                const pageData = childPages[folderName];
-                if (!pageData) continue;
-                //
-                const aElement = document.createElement("a");
-                listElement.appendChild(aElement);
-                aElement.href = folderName + "/index.html";
-                //
-                const blockElement = document.createElement("div");
-                blockElement.classList.add("blog_block");
-                blockElement.classList.add("neumorphism");
-                aElement.appendChild(blockElement);
-                //
-                const headlineElement = document.createElement("h3");
-                blockElement.appendChild(headlineElement);
-                headlineElement.innerText = pageData?.headline;
-                //
-                const pElement = document.createElement("p");
-                blockElement.appendChild(pElement);
-                pElement.innerText = pageData?.overview;
+            async function aaa() {
+                while (!settings[0]) {
+                    await sleep(500);
+                }
+                listElement.innerHTML = "";
+                const childPages = {
+                    ...(settings[0]?.childPages ?? {}),
+                };
+                for (const folderName in childPages) {
+                    const pageData = childPages[folderName];
+                    if (!pageData) continue;
+                    //
+                    const aElement = document.createElement("a");
+                    listElement.appendChild(aElement);
+                    aElement.href = folderName + "/index.html";
+                    //
+                    const blockElement = document.createElement("div");
+                    blockElement.classList.add("blog_block");
+                    blockElement.classList.add("neumorphism");
+                    aElement.appendChild(blockElement);
+                    //
+                    const headlineElement = document.createElement("h3");
+                    blockElement.appendChild(headlineElement);
+                    headlineElement.innerText = pageData?.headline;
+                    //
+                    const pElement = document.createElement("p");
+                    blockElement.appendChild(pElement);
+                    pElement.innerText = pageData?.overview;
+                }
             }
+            aaa();
             return listElement;
         },
         "renderLight": async function (blockId, saveData) {
